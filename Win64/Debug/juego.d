c7b728d02d447c2728ce8e19bfb91483231951a6
FaustoFrancisco <fausto.francisco.solis@gmail.com>
2017-04-08 19:28:26 -0500

    V1
    Carga de archivos de la version 1 del juego de la vida

---
+++ Win64/Debug/juego.d
@@ -0,0 +1,8 @@
+.\Win64\Debug\juego.o: juego.cpp \
+  c:\program\ files\ (x86)\embarcadero\studio\18.0\include\windows\sdk\windows.h \
+  c:\program\ files\ (x86)\embarcadero\studio\18.0\include\windows\crtl\time.h \
+  C:\Users\francisco\Documents\Embarcadero\Studio\Projects/juego.h \
+  c:\program\ files\ (x86)\embarcadero\studio\18.0\include\windows\vcl\Vcl.Grids.hpp \
+  c:\program\ files\ (x86)\embarcadero\studio\18.0\include\windows\vcl\Vcl.Mask.hpp \
+  c:\program\ files\ (x86)\embarcadero\studio\18.0\include\windows\rtl\System.MaskUtils.hpp \
+  c:\program\ files\ (x86)\embarcadero\studio\18.0\include\windows\vcl\Vcl.Buttons.hpp
